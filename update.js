906ff9af86dbf94cd7ff89789c34cdd26ee38705
baldbuffalo <64604745+baldbuffalo@users.noreply.github.com>
2023-11-11 15:25:36 +0400

    Create update.js

---
+++ update.js
@@ -0,0 +1,17 @@
+// Update the URL with your Netlify site and function name
+const authEndpoint = 'https://your-netlify-site.netlify.app/.netlify/functions/auth';
+
+// Perform Google Sign-In and send user data to the serverless function
+// ...
+
+// Example using fetch
+fetch(authEndpoint, {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({ googleId, displayName, email }),
+})
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
