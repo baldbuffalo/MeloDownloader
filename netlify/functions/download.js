e9ed850087c03f69e1f884af1f9282810b3856ba
baldbuffalo <64604745+baldbuffalo@users.noreply.github.com>
2023-11-11 20:13:51 +0400

    Update download.js

--- netlify/functions/download.js
+++ netlify/functions/download.js
@@ -1,61 +1,29 @@
-// functions/download.js
-
-const fetch = require('node-fetch');
 const ytdl = require('ytdl-core');
-const { Readable } = require('stream');
 
 exports.handler = async function (event, context) {
+  const youtubeLink = event.queryStringParameters.youtubeLink;
+
   try {
-    const { youtubeLink } = event.queryStringParameters;
-
-    if (!youtubeLink) {
-      return {
-        statusCode: 400,
-        body: JSON.stringify({ error: 'YouTube link is required.' }),
-      };
-    }
-
-    // Validate YouTube link (you might want to add more validation)
-    if (!ytdl.validateURL(youtubeLink)) {
-      return {
-        statusCode: 400,
-        body: JSON.stringify({ error: 'Invalid YouTube link.' }),
-      };
-    }
-
-    // Get video information
+    // Use ytdl-core to get the video stream
     const videoInfo = await ytdl.getInfo(youtubeLink);
-
-    // Get the highest quality video stream
     const videoStream = ytdl(youtubeLink, { quality: 'highestvideo' });
 
-    // Convert video stream to a buffer
-    const videoBuffer = await streamToBuffer(videoStream);
+    // Set headers for video download
+    const headers = {
+      'Content-Disposition': `attachment; filename="${videoInfo.videoDetails.title}.mp4"`,
+      'Content-Type': 'video/mp4',
+    };
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ videoBuffer, videoInfo }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
+      headers,
+      body: videoStream,
+      isBase64Encoded: true,
     };
   } catch (error) {
-    console.error('Error:', error);
-
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: 'Internal Server Error' }),
+      body: JSON.stringify({ error: 'Error downloading the video' }),
     };
   }
 };
-
-// Utility function to convert a stream to a buffer
-async function streamToBuffer(stream) {
-  return new Promise((resolve, reject) => {
-    const chunks = [];
-    stream.on('data', (chunk) => chunks.push(chunk));
-    stream.on('end', () => resolve(Buffer.concat(chunks)));
-    stream.on('error', (error) => reject(error));
-  });
-}
-
