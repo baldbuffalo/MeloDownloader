8183ddb7ba5d6cafcda3235e766bdbb6abe0e006
baldbuffalo <64604745+baldbuffalo@users.noreply.github.com>
2023-11-11 15:27:53 +0400

    Create auth.js

---
+++ netlify/functions/auth.js
@@ -0,0 +1,22 @@
+// netlify/functions/auth.js
+
+exports.handler = async function(event, context) {
+  try {
+    // Parse the incoming data from the client
+    const { googleId, displayName, email } = JSON.parse(event.body);
+
+    // Perform server-side logic (e.g., store data in a database)
+
+    // Respond with a success message
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Data received and processed successfully' }),
+    };
+  } catch (error) {
+    // Handle errors
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Internal server error' }),
+    };
+  }
+};
